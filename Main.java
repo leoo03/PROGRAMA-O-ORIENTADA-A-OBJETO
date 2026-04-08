95af636f708b6436d3b3406ab80ed1996b7e9435
Leonidas <leonidas6franca5@gmail.com>
2026-04-08 16:48:13 -0300

    Implement main method to manage Carro instances

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+  public void main(){
+
+      Carro carro = new Carro("GOLF","G4",2015,"PRATA",80,3);
+
+      carro.acelerar();
+      carro.freiar();
+      carro.freiar();
+      carro.vender();
+      carro.vender();
+      carro.exibirInformacoes();
+
+      Carro carro1 = new Carro("FISTA","FORD",2014,"PRETO",150,10);
+
+
+      carro1.acelerar();
+      carro1.freiar();
+      carro1.freiar();
+      carro1.freiar();
+      carro1.vender();
+      carro1.vender();
+      carro1.exibirInformacoes();
+
+
+
+  }
+
+
+}
+
